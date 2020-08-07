4625aed7d8f2d64d853b727d6552422e66110ea3
CleytonPinheiro <cleyton_pin@hotmail.com>
2020-08-07 11:59:52 -0300

    Deixando os dados dinâmicos trabalhando com o nunjucks na page study.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const proffys = [
     {
-        name: "Diego Fernandes",
-        avatar:"D\"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        name: "Mayk Brito",
+        avatar:"https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
         whatsapp:"44-88996655",
         bio:"Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões",
         subject:"Química",
@@ -11,8 +11,8 @@ const proffys = [
         time_to: [1220]
     },
     {
-        name: "Daniela Evangelista",
-        avatar:"D\"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        name: "Mayk Brito",
+        avatar:"https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
         whatsapp:"44-88996655",
         bio:"Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões",
         subject:"Química",
@@ -24,15 +24,15 @@ const proffys = [
 ]
 
 function pageLanding(req, res) {
-    return res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 }
 
 function pageStudy(req, res) {
-    return res.sendFile(__dirname + "/views/study.html")
+    return res.render("study.html", { proffys })
 }
 
 function pageGiveClasses(req, res) {
-    return res.sendFile(__dirname + "/views/give-classes.html")
+    return res.render("give-classes.html")
 }
 
 
